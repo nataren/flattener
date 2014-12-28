0dba0d1350d35aad97d83f168f71e6edd2b776a2
César López-Natarén <cesarn@mindtouch.com>
2014-12-28 11:23:24 -0800

    Better names, and format changes

--- flattener.go
+++ flattener.go
@@ -1,48 +1,76 @@
 package main
 
 import (
+	"bufio"
 	"encoding/xml"
 	"fmt"
-	"os"
-	"bufio"
 	"log"
+	"os"
 )
 
 func main() {
 	type Signature struct {
-		SignatureString string `xml:",innerxml"`
+		XMLName xml.Name `xml:"signature"`
+		Value   string   `xml:",innerxml"`
 	}
 
 	type IP struct {
-		IPString string `xml:",innerxml"`
+		XMLName xml.Name `xml:"ip"`
+		Value   string   `xml:",innerxml"`
 	}
 
 	type SessionId struct {
-		SessionIdString string `xml:",innerxml"`
+		XMLName xml.Name `xml:"session-id"`
+		Value   string   `xml:",innerxml"`
+	}
+
+	type User struct {
+		XMLName   xml.Name `xml:"user"`
+		Id        string   `xml:"id,attr"`
+		Anonymous string   `xml:"anonymous,attr"`
 	}
 
 	type Request struct {
-		Id string `xml:"id,attr"`
-		Seq string `xml:"seq,attr"`
-		Count string `xml:"count,attr"`
+		XMLName   xml.Name  `xml:"request"`
+		Id        string    `xml:"id,attr"`
+		Seq       string    `xml:"seq,attr"`
+		Count     string    `xml:"count,attr"`
 		Signature Signature `xml:"signature"`
-		IP IP `xml:"ip"`
+		IP        IP        `xml:"ip"`
 		SessionId SessionId `xml:"session-id"`
-		// Parameters []string `xml:"request>parameters"`
+		// Parameters []string `xml:"parameters"`
+		User User `xml:"user"`
+	}
+
+	type PagePath struct {
+		XMLName xml.Name `xml:"path"`
+		Value   string   `xml:",innerxml"`
+	}
+
+	type Page struct {
+		XMLName xml.Name `xml:"page"`
+		Id      string   `xml:"id,attr"`
+		Path    PagePath `xml:"path"`
+	}
+
+	type Data struct {
+		XMLName   xml.Name `xml:"data"`
+		UriHost   string   `xml:"_uri.host"`
+		UriScheme string   `xml:"_uri.scheme"`
+		UriQuery  string   `xml:"_uri.query"`
 	}
 
 	type MindTouchEvent struct {
-		XMLName xml.Name `xml:"event"`
-		Id  string  `xml:"id,attr"`
-		Datetime string `xml:"datetime,attr"`
-		Type string `xml:"type,attr"`
-		Wikiid string `xml:"wikiid,attr"`
-		Journaled string `xml:"journaled,attr"`
-		Version string `xml:"version,attr"`
-		Request Request `xml:"request"`
-		// RequestUser
-		// PageId
-		// PagePath string `xml:"page>path"`
+		XMLName   xml.Name `xml:"event"`
+		Id        string   `xml:"id,attr"`
+		Datetime  string   `xml:"datetime,attr"`
+		Type      string   `xml:"type,attr"`
+		Wikiid    string   `xml:"wikiid,attr"`
+		Journaled string   `xml:"journaled,attr"`
+		Version   string   `xml:"version,attr"`
+		Request   Request  `xml:"request"`
+		Page      Page     `xml:"page"`
+		Data      Data     `xml:"data"`
 	}
 
 	// Open file
@@ -63,11 +91,11 @@ func main() {
 		if event == "" {
 			break
 		}
-		ev := MindTouchEvent { }
-        unmarshalErr := xml.Unmarshal([]byte(event), &ev)
-        if unmarshalErr != nil {
-	        log.Printf("Could not deserialize: '%s'", event)
-        }
-        fmt.Println(ev)
+		ev := MindTouchEvent{}
+		unmarshalErr := xml.Unmarshal([]byte(event), &ev)
+		if unmarshalErr != nil {
+			log.Printf("Could not deserialize: '%s'", event)
+		}
+		fmt.Println(ev)
 	}
 }
